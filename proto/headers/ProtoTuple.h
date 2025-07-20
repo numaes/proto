3cd284518d08d94395224b1b13243ddb93b615dd
Gustavo Marino <gamarino@numaes.com>
2025-07-20 19:43:14 -0300

    feat: Compila el proyecto y corrige el Makefile
    Se realizaron los siguientes cambios para que el proyecto compile correctamente:
    
    - Se corrigió el Makefile para que encuentre los encabezados en el directorio .
    - Se agregó  a la lista de encabezados.
    - Se cambió el compilador de C a  para que sea consistente con el de C++.
    - Se corrigió la regla de enlazado para el ejecutable de prueba.
    - Se agregó un archivo  con una función  vacía.
    - Se agregaron al repositorio los archivos  y  que no estaban versionados.

---
+++ proto/headers/ProtoTuple.h
@@ -0,0 +1,15 @@
+/*
+ *  ProtoTuple.h
+ *  See Copyright Notice in proto.h
+ */
+
+#ifndef PROTOTUPLE_H
+#define PROTOTUPLE_H
+
+#include "proto.h"
+
+namespace proto {
+    class ProtoTupleImplementation;
+}
+
+#endif
